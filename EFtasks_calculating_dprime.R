0dd77138eab483c6835f6324324467c4d339a7a5
Ursula Tooley <utooley@gmail.com>
2016-10-07 17:18:26 -0400

    Added coding for dprime for VNB

--- EFtasks_calculating_dprime.R
+++ EFtasks_calculating_dprime.R
@@ -1,6 +1,7 @@
 library(psych)
 library(dplyr)
 #import behavioral data
+setwd("/Users/josephkable/Desktop/Ursula/Kable Lab/CogTrain/")
 prebeh <- read.csv("DMP_pretx_demo_tasks_withnewvars_12.15.15.csv", sep = ",", header = TRUE)
 colnames(prebeh)
 #filter out only n-back and cpt columns
@@ -9,19 +10,53 @@ colnames(vnb)
 cpt <- prebeh %>% select(contains("cpt"))
 colnames(cpt)
 
-#make vnb total number of trials for each participant
-vnb <- mutate(vnb,vnb_tot_trials=vnb_fp_ct_all_pretx+ vnb_tp_ct_all_pretx + 
-         vnb_fn_ct_0_pretx+ vnb_fn_ct_1_pretx+ vnb_fn_ct_2_pretx + vnb_fn_ct_3_pretx)
+# Visual N-Back -----------------------------------------------------------
 
-#make cpt total number of trials for each participant
-cpt <- mutate(cpt, cpt_total_trials=cpt_fp_ct_pretx+cpt_fn_ct_pretx+cpt_tp_ct_pretx)
-vnb$vnb_tot_trials
-cpt$cpt_total_trials
-#see max min 
-describe(cpt$cpt_total_trials)
-describe(vnb$vnb_tot_trials)
+#Total target trials for VNB=60, Non-target trials=140
+vnb$vnb_hit=(vnb$vnb_tp_ct_all_pretx)/60
+vnb$vnb_fa=(vnb$vnb_fp_ct_all_pretx)/140
 
-#
+#replace max and min values (0's and 1's) with 1-1/2n for hits or 1/2n for fa according to Haatveit et al 2009
+#n=num of targets/non-targets
+nh=1-1/(2*60)
+nfa=1/(2*140)
+vnb$vnb_hit <- replace(vnb$vnb_hit, vnb$vnb_hit==1, nh)
+vnb$vnb_fa <- replace(vnb$vnb_fa, vnb$vnb_fa==0, nfa)
+vnb$
+#look for this percentage on the Z distribution
+vnb$vnb_hit_z=qnorm(vnb$vnb_hit)
+vnb$vnb_fa_z=qnorm(vnb$vnb_fa)
+
+vnb$vnb_fa_z
+vnb$vnb_hit_z
+#create a d-prime measure
+vnb$vnb_ct_dprime=vnb$vnb_hit_z-vnb$vnb_fa_z
+describe(vnb$vnb_ct_dprime)
+
+
+# Continous Performance (CPT) ---------------------------------------------
+
+#Total target trials for VNB=60, Non-target trials=140
+vnb$vnb_hit=(vnb$vnb_tp_ct_all_pretx)/60
+vnb$vnb_fa=(vnb$vnb_fp_ct_all_pretx)/140
+
+#replace max and min values (0's and 1's) with 1-1/2n for hits or 1/2n for fa according to Haatveit et al 2009
+#n=num of targets/non-targets
+nh=1-1/(2*60)
+nfa=1/(2*140)
+vnb$vnb_hit <- replace(vnb$vnb_hit, vnb$vnb_hit==1, nh)
+vnb$vnb_fa <- replace(vnb$vnb_fa, vnb$vnb_fa==0, nfa)
+vnb$
+
+#look for this percentage on the Z distribution
+vnb$vnb_hit_z=qnorm(vnb$vnb_hit)
+vnb$vnb_fa_z=qnorm(vnb$vnb_fa)
+
+vnb$vnb_fa_z
+vnb$vnb_hit_z
+#create a d-prime measure
+vnb$vnb_ct_dprime=vnb$vnb_hit_z-vnb$vnb_fa_z
+describe(vnb$vnb_ct_dprime)
 
 #write new data file for future use
 write(prebeh, 
