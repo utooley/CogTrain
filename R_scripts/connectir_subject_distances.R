af76df2583c510468fe42d420307d5089e94a2c8
Ursula Tooley <utooley@gmail.com>
2016-10-19 14:11:10 -0400

    started work on connectir script

--- R_scripts/connectir_subject_distances.R
+++ R_scripts/connectir_subject_distances.R
@@ -1,7 +1,11 @@
+
+
 connectir_subdist.R
 -i functional_path_list.txt \
 --automask1 \
 --brainmask1 standard_grey_matter.nii.gz \
+#here use Wager mask 4rgray_matter_mask_binary
 --bg standard_brain_4mm.nii.gz \
+#here use standard brain from SPM, /spm12/canonical/single_subj_T1.nii
 --memlimit 20 -c 3 -t 4 \
 subject_distances_outdir
