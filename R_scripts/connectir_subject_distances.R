22d8c9fecb456ad1f77ff5ea618bbb992b6591e6
Ursula Tooley <utooley@gmail.com>
2016-10-20 19:07:55 -0400

    What works in the terminal to calculate sub. distances

--- R_scripts/connectir_subject_distances.R
+++ R_scripts/connectir_subject_distances.R
@@ -1,11 +1,22 @@
-
+library(connectir)
 
 connectir_subdist.R
--i functional_path_list.txt \
+-i functional_images_list.txt \
 --automask1 \
---brainmask1 standard_grey_matter.nii.gz \
-#here use Wager mask 4rgray_matter_mask_binary
---bg standard_brain_4mm.nii.gz \
-#here use standard brain from SPM, /spm12/canonical/single_subj_T1.nii
+--brainmask1 GreyMask_v2_4mm.nii \
+--bg single_subj_T1.nii \
 --memlimit 20 -c 3 -t 4 \
-subject_distances_outdir
+/Users/utooley/Documents/Kable Lab/CogTrain/image_data/test
+
+setwd("/Users/utooley/Documents/Kable Lab/CogTrain/CogTrain/R_scripts/connectir-master/inst/scripts/")
+
+#brainmask1:
+#here use Wager mask 4rgray_matter_mask_binary
+#standard brain here
+#here use standard brain from SPM, /spm12/canonical/single_subj_T1.nii
+
+#paste this in the terminal and it looks like it's starting to work!
+#however, haven't gotten anywhere yet
+#stuck on GreyMask_v2_4mm.nii being a 3D image and not 4 D
+
+Rscript connectir_subdist.R -i functional_images_list.txt --automask1 --brainmask1 test/GreyMask_v2_4mm.nii --bg test/single_subj_T1.nii --memlimit 20 /Users/utooley/Documents/Kable Lab/CogTrain/image_data/test
--- matlab_scripts/resampling_voxels_4x4x4.m
+++ matlab_scripts/resampling_voxels_4x4x4.m
@@ -1,5 +1,5 @@
 voxsiz = [4 4 4]; % new voxel size {mm}
-V = spm_select([1 Inf],'Image');
+V = spm_select([1 Inf],'Filtered_4DVolume.nii');
 V = spm_vol(V);
 for i=1:numel(V)
    bb        = spm_get_bbox(V(i));
