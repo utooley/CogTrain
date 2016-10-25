dfb88df8d5d32fc3a85dbf1604f2ed51dec92e4f
Ursula Tooley <utooley@gmail.com>
2016-10-25 11:39:16 -0400

    Updating subject distances to make new tries at running through terminal.

--- R_scripts/connectir_subject_distances.R
+++ R_scripts/connectir_subject_distances.R
@@ -19,4 +19,18 @@ setwd("/Users/utooley/Documents/Kable Lab/CogTrain/CogTrain/R_scripts/connectir-
 #however, haven't gotten anywhere yet
 #stuck on GreyMask_v2_4mm.nii being a 3D image and not 4 D
 
-Rscript connectir_subdist.R -i functional_images_list.txt --automask1 --brainmask1 test/GreyMask_v2_4mm.nii --bg test/single_subj_T1.nii --memlimit 20 /Users/utooley/Documents/Kable Lab/CogTrain/image_data/test
+Rscript connectir_subdist.R -i test/functional_images_list.txt --automask1 --brainmask1 test/GreyMask_v2_4mm.nii --bg test/single_subj_T1.nii --memlimit 20 /Users/utooley/Documents/tes1t
+
+#test whether voxels in mask are true somewhere
+table(inlist1$mask)["TRUE"]
+#some are!
+
+
+##Trying to put in command line arguments and then run in R
+infuncs1="test/functional_images_list.txt"
+automask1=TRUE
+brainmask1="test/GreyMask_v2_4mm.nii"
+bg= "test/single_subj_T1.nii"
+memlimit = c(20)
+outdir ="/Users/utooley/Documents/tes1t"
+source("connectir_subdist_worker.R")
