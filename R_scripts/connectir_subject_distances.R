a0e3498c748b0ea86afb0771e5fe1c815ca57054
Ursula Tooley <utooley@gmail.com>
2016-11-03 11:05:27 -0400

    Fixed save location, z-transform.

--- R_scripts/connectir_subject_distances.R
+++ R_scripts/connectir_subject_distances.R
@@ -1,15 +1,7 @@
 library(connectir)
 
 
-##REAL TERMINAL INPUT FOR ALL IMAGES ON PERSONAL COMPUTER
-cd /Users/utooley/Documents/Kable_Lab/CogTrain/CWAS
-/Users/utooley/Documents/Kable_Lab/CogTrain/CogTrain/R_scripts/connectir-master/inst/scripts/connectir_subdist.R \
--i functional_images_list_wo_outliers.txt \
---automask1 \
---brainmask1 GreyMask_v2_4mm.nii \
---bg single_subj_T1.nii \ 
---memlimit 35 \
-/Users/utooley/Kable_Lab/CogTrain/CWAS/subdist \
+##REAL TERMINAL INPUT FOR ALL IMAGES ON PERSONAL COMPUTER IS IN BASH SCRIPT
 
 
 connectir_subdist.R \
--- bash_scripts/calculating_subdist_inputs_zscored.sh
+++ bash_scripts/calculating_subdist_inputs_zscored.sh
@@ -1,10 +1,9 @@
-##REAL TERMINAL INPUT FOR ALL IMAGES ON PERSONAL COMPUTER
-
 #!/bin/bash 
 cd /Users/utooley/Documents/Kable_Lab/CogTrain/CWAS
 /Users/utooley/Documents/Kable_Lab/CogTrain/CogTrain/R_scripts/connectir-master/inst/scripts/connectir_subdist.R \
 -i functional_images_list_wo_outliers.txt \
 --automask1 \
+--memlimit 35 \
+--ztransform \
 --brainmask1 GreyMask_v2_4mm.nii \
---bg single_subj_T1.nii \ 
---memlimit 35 \ sub_dist_with_zscored
+--bg single_subj_T1.nii sub_dist_with_zscored
