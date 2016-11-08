797f148f161f046da3e97b4d69274fdd344718d5
Ursula Tooley <utooley@gmail.com>
2016-11-08 13:53:26 -0500

    new script

--- bash_scripts/mdmr_no_updating_or_IQ.sh
+++ bash_scripts/mdmr_no_updating_or_IQ.sh
@@ -1,11 +1,24 @@
-cd /Users/utooley/Documents/Kable_Lab/CogTrain/CWAS
-Rscript ./connectir_mdmr.R -i subj_dist_with_zscore \
+cd /data/jet/kable/Ursula/
+Rscript /home/utooley/connectir-master/inst/scripts/connectir_mdmr.R -i sub_dist_with_zscored \
     --formula "age + sex + mean.FD + composite + csh_residual_variance" \
     --model demo_and_EF_MDMR_regressors_110416.csv \
     --factors2perm "composite, csh_residual_variance" \
     --permutations 14999 \
-    --forks 1 --threads 12 \
-    --memlimit 15 \
+    --forks 4 \
+    --memlimit 40 \
     --save-perms \
     --ignoreprocerror \
     mdmr_age+sex+mean.FD+composite+switch.mdmr
+    
+##interactive w/ just 4 subjects
+cd /data/jet/kable/Ursula/
+Rscript /home/utooley/connectir-master/inst/scripts/connectir_mdmr.R -i sub_dist_with_zscored_t2 \
+    --formula "age + sex + mean.FD + composite" \
+    --model demo_and_EF_MDMR_regressors_110416_4sub.csv \
+    --factors2perm "composite" \
+    --permutations 14999 \
+    --forks 4 \
+    --memlimit 40 \
+    --save-perms \
+    --ignoreprocerror \
+mdmr_age+sex+mean.FD+composite+switch_4sub.mdmr
