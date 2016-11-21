17605cbad8442ff730c5723fcdfd9831e9c88fd6
Ursula Tooley <utooley@gmail.com>
2016-11-21 16:44:30 -0500

    Adding cluster extraction with 3dExtrema and cluster.

--- bash_scripts/permutation_based_cluster_correction.sh
+++ bash_scripts/permutation_based_cluster_correction.sh
@@ -9,3 +9,12 @@ cd /home/utooley/connectir-master/permutation/
 Rscript ~/connectir-master/permutation/le_correcter.R /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg/age+sex+meanFD+composite+update+switch_allpermuted.mdmr
 
 #Now extract with 3dExtrema or with cluster?
+cluster --zstat=cluster_correct_v05_c05/clust_logp_composite.nii -t 1
+
+cd /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg/age+sex+meanFD+composite+update+switch.mdmr
+3dExtrema -prefix extrem -volume -closure cluster_correct_v05_c05/clust_logp_composite.nii 
+
+#with sep dist of 20 mm
+3dExtrema -prefix extrem -volume -sep_dist 20 -closure cluster_correct_v05_c05/clust_logp_composite.nii 
+
+#peaks on boundary or no= with or without -closure 
