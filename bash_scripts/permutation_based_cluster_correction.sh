32dc6013a1891dd04d4c94b7ec98664c2463693c
Ursula Tooley <utooley@gmail.com>
2016-11-21 14:27:20 -0500

    Permutation-based cluster correct.

--- bash_scripts/permutation_based_cluster_correction.sh
+++ bash_scripts/permutation_based_cluster_correction.sh
@@ -1,8 +1,11 @@
+#composite only no covariates
 cd /home/utooley/connectir-master/permutation/
 Rscript ~/connectir-master/permutation/le_correcter.R /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg/age+sex+meanFD+composite.mdmr
-
+#composite w/ covariates
 cd /home/utooley/connectir-master/permutation/
 Rscript ~/connectir-master/permutation/le_correcter.R /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg/age+sex+meanFD+composite+update+switch.mdmr
-
+#all permuted
 cd /home/utooley/connectir-master/permutation/
 Rscript ~/connectir-master/permutation/le_correcter.R /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg /data/jet/kable/Ursula/sub_dist_with_zscored_4mmbg/age+sex+meanFD+composite+update+switch_allpermuted.mdmr
+
+#Now extract with 3dExtrema or with cluster?
